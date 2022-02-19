33b848930d247ba3e59d763acb20b7141056bde6
pfredt1 <pfredt@gmail.com>
2022-02-19 11:21:07 -0500

    display images on cards

--- ui.py
+++ ui.py
@@ -1,11 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -18,7 +20,8 @@ class QuizInterface:
             150, 125,
             text="some text here",
             fill=THEME_COLOR,
-            font=('Arial', 20, "italic"))
+            font=('Arial', 20, "italic"),
+            width=280)
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_image = PhotoImage(file='images/true.png')
@@ -28,9 +31,14 @@ class QuizInterface:
         false_image = PhotoImage(file='images/false.png')
         self.false_button = Button(image=false_image, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
-
+        self.get_next_question()
 
 
 
 
         self.window.mainloop()
+    def get_next_question(self):
+        q_qtext = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_qtext)
+
+
